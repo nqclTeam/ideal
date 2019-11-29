e38cadada4e31b6fbfda6b6311c4fc2c70753ef4
zhangv0517 <zhangv_job@163.com>
2019-11-29 16:24:52 +0800

    bean generator

--- src/main/java/com/nqcl/api/dao/UserMapper.java
+++ src/main/java/com/nqcl/api/dao/UserMapper.java
@@ -1,11 +1,11 @@
 package com.nqcl.api.dao;
 
 import com.nqcl.api.entity.User;
-import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
+@Repository
 //注意：别忘了此注解 注入Bean
 public interface UserMapper {
 
--- src/main/java/com/nqcl/api/entity/User.java
+++ src/main/java/com/nqcl/api/entity/User.java
@@ -1,23 +1,11 @@
 package com.nqcl.api.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
 public class User {
     private Integer id;
     private String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
