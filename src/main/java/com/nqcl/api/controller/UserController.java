c76d005dde59f2cbe8d0c627d0192a5ba30917f4
hidetou <2675514981@qq.com>
2019-11-29 14:11:56 +0800

    环境搭

--- src/main/java/com/nqcl/api/controller/UserController.java
+++ src/main/java/com/nqcl/api/controller/UserController.java
@@ -6,23 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("user")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<User> hello(){
-        List<User> hello=null;
+    @RequestMapping(value = "user", method = RequestMethod.GET)
+    public List<User> hello() {
+        List<User> hello = null;
         try {
-            hello = userService.hello( );
-        }catch (Exception e){
-            System.out.println(e );
+            hello = userService.hello();
+        } catch (Exception e) {
+            System.out.println(e);
         }
         return hello;
+    }
+
+    @RequestMapping(value = "index1", method = RequestMethod.GET)
+    public ModelAndView getUserList() {
+        ModelAndView mav = new ModelAndView();
+        try {
+            List<User> users = userService.hello();
+            mav.addObject("users", users);
+            mav.setViewName("index");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mav;
+    }
+
+    @RequestMapping("index2")
+    public String toIndex(){
+        return "index.html";
     }
 }
